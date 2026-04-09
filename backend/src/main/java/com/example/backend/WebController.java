70b9a5b91a99fe5f9cb0dc0ddb87e998f3e3cda1
kuuharuh <jortizarmas@gmail.com>
2026-04-09 10:35:36 +0200

    Añadir páginas básicas al Controller e implementar navegación

--- backend/src/main/java/com/example/backend/WebController.java
+++ backend/src/main/java/com/example/backend/WebController.java
@@ -9,4 +9,34 @@ public class WebController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/item-detail")
+    public String itemDetail() {
+        return "pages/item-detail";
+    }
+
+    @GetMapping("/shopping-cart")
+    public String shoppingCart() {
+        return "pages/shopping-cart";
+    }
+
+    @GetMapping("/create-review")
+    public String createReview() {
+        return "pages/create-review";
+    }
+
+    @GetMapping("/search-result")
+    public String searchResult() {
+        return "pages/search-result";
+    }
+
+    @GetMapping("/payment")
+    public String payment() {
+        return "pages/payment";
+    }
+
+    @GetMapping("/payment-correct")
+    public String paymentCorrect() {
+        return "pages/payment_correct";
+    }
 }
