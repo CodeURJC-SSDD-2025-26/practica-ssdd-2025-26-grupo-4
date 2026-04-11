d50bbba2d8d87e7e268913b69a8d9f47bb4d34fa
Ramiro <daniel.flo.aqu@gmail.com>
2026-04-11 19:23:56 +0200

    Quitar_Comentarios

--- backend/src/main/java/com/example/backend/security/RepositoryUserDetailsService.java
+++ backend/src/main/java/com/example/backend/security/RepositoryUserDetailsService.java
@@ -22,19 +22,16 @@ public class RepositoryUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // 1. Buscamos al usuario en tu base de datos
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
 
-        // 2. Transformamos tu lista de roles ["ROLE_USER"] a los permisos de Spring
         List<GrantedAuthority> authorities = user.getRoles().stream()
                 .map(role -> new SimpleGrantedAuthority(role))
                 .collect(Collectors.toList());
 
-        // 3. Devolvemos el usuario "traducido"
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
-                user.getEncodedPassword(), // Usa la variable de tu clase User
+                user.getEncodedPassword(), 
                 authorities
         );
     }
