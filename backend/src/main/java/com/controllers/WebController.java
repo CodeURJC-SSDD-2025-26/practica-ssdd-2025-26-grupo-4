5be696ac1e7541ca8c727f65e84dd99b20b3d7e1
Ramiro <daniel.flo.aqu@gmail.com>
2026-04-11 19:06:54 +0200

    Controlls_ADMIN

--- backend/src/main/java/com/controllers/WebController.java
+++ backend/src/main/java/com/controllers/WebController.java
@@ -54,4 +54,49 @@ public class WebController {
     public String profile() {
         return "pages/profile";
     }
+    @GetMapping("/admin/admin-dashboard")
+    public String adminDashboard(){
+        return "pages/admin/admin-dashboard";
+    }
+    @GetMapping("/admin/item-create")
+    public String itemCreate(){
+        return "pages/admin/item-create";
+    }
+    @GetMapping("/admin/item-edit")
+    public String itemEdit(){
+        return "pages/admin/item-edit";
+    }
+    @GetMapping("/admin/item-list")
+    public String itemList(){
+        return "pages/admin/item-list";
+    }
+    @GetMapping("/admin/order-list")
+    public String orderList(){
+        return "pages/admin/order-list";
+    }
+    @GetMapping("/admin/order-edit")
+    public String orderEdit(){
+        return "pages/admin/order-edit";
+    }
+    @GetMapping("/admin/order-management")
+    public String orderManagement(){
+        return "pages/admin/order-management";
+    }
+    @GetMapping("/admin/review-list")
+    public String reviewList(){
+        return "pages/admin/review-list";
+    }
+    @GetMapping("/admin/user-create")
+    public String userCreate(){
+        return "pages/admin/user-create";
+    }
+    @GetMapping("/admin/user-edit")
+    public String userEdit(){
+        return "pages/admin/user-edit";
+    }
+    @GetMapping("/admin/user-list")
+    public String userList(){
+        return "pages/admin/user-list";
+    }
+    
 }
